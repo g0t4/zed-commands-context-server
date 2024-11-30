31853aad1caf8eff5d74c681a668a2b589b16548
Wes Higbee <wes.mcclure@gmail.com>
2024-11-29 18:44:30 -0600

    copy other for ref

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+use serde::Deserialize;
+use std::env;
+use zed::settings::ContextServerSettings;
+use zed_extension_api::{self as zed, serde_json, Command, ContextServerId, Project, Result};
+
+const PACKAGE_NAME: &str = "@zeddotdev/postgres-context-server";
+const PACKAGE_VERSION: &str = "0.1.2";
+const SERVER_PATH: &str = "node_modules/@zeddotdev/postgres-context-server/index.mjs";
+
+struct PostgresModelContextExtension;
+
+#[derive(Debug, Deserialize)]
+struct PostgresContextServerSettings {
+    database_url: String,
 }
+
+impl zed::Extension for PostgresModelContextExtension {
+    fn new() -> Self {
+        Self
+    }
+
+    fn context_server_command(
+        &mut self,
+        _context_server_id: &ContextServerId,
+        project: &Project,
+    ) -> Result<Command> {
+        let version = zed::npm_package_installed_version(PACKAGE_NAME)?;
+        if version.as_deref() != Some(PACKAGE_VERSION) {
+            zed::npm_install_package(PACKAGE_NAME, PACKAGE_VERSION)?;
+        }
+
+        let settings = ContextServerSettings::for_project("postgres-context-server", project)?;
+        let Some(settings) = settings.settings else {
+            return Err("missing `database_url` setting".into());
+        };
+        let settings: PostgresContextServerSettings =
+            serde_json::from_value(settings).map_err(|e| e.to_string())?;
+
+        Ok(Command {
+            command: "node".to_string(),
+            args: vec![
+                env::current_dir()
+                    .unwrap()
+                    .join(SERVER_PATH)
+                    .to_string_lossy()
+                    .to_string(),
+            ],
+            env: vec![
+                ("DATABASE_URL".into(), settings.database_url)
+            ],
+        })
+    }
+}
+
+zed::register_extension!(PostgresModelContextExtension);
+
