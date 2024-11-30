10e828c8380d39c10e20027bb77c510c0e0cae38
Wes Higbee <wes.mcclure@gmail.com>
2024-11-29 18:46:20 -0600

    no settings and other cleanup of unused

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use serde::Deserialize;
 use std::env;
-use zed::settings::ContextServerSettings;
-use zed_extension_api::{self as zed, serde_json, Command, ContextServerId, Project, Result};
+use zed_extension_api::{self as zed, Command, ContextServerId, Project, Result};
 
 const PACKAGE_NAME: &str = "@zeddotdev/postgres-context-server";
 const PACKAGE_VERSION: &str = "0.1.2";
@@ -9,10 +7,7 @@ const SERVER_PATH: &str = "node_modules/@zeddotdev/postgres-context-server/index
 
 struct CommandsModelContextExtension;
 
-#[derive(Debug, Deserialize)]
-struct PostgresContextServerSettings {
-    database_url: String,
-}
+// TODO consider using shell type setting (or other settings), see postgres-context-server for using settings
 
 impl zed::Extension for CommandsModelContextExtension {
     fn new() -> Self {
@@ -22,19 +17,13 @@ impl zed::Extension for CommandsModelContextExtension {
     fn context_server_command(
         &mut self,
         _context_server_id: &ContextServerId,
-        project: &Project,
+        _project: &Project,
     ) -> Result<Command> {
         let version = zed::npm_package_installed_version(PACKAGE_NAME)?;
         if version.as_deref() != Some(PACKAGE_VERSION) {
             zed::npm_install_package(PACKAGE_NAME, PACKAGE_VERSION)?;
         }
 
-        let settings = ContextServerSettings::for_project("postgres-context-server", project)?;
-        let Some(settings) = settings.settings else {
-            return Err("missing `database_url` setting".into());
-        };
-        let settings: PostgresContextServerSettings =
-            serde_json::from_value(settings).map_err(|e| e.to_string())?;
 
         Ok(Command {
             command: "node".to_string(),
@@ -46,7 +35,7 @@ impl zed::Extension for CommandsModelContextExtension {
                     .to_string(),
             ],
             env: vec![
-                ("DATABASE_URL".into(), settings.database_url)
+            //    ("DATABASE_URL".into(), settings.database_url)
             ],
         })
     }
